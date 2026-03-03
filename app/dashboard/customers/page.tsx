a21cd9906ddc16687b6a16259fdd7a24a54f0e95
Kyrylo Osypov <kosypov@starta.one>
2026-03-03 19:03:54 +0200

    fix

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,3 @@
-import Pagination from '@/app/ui/invoices/pagination';
-import Search from '@/app/ui/search';
-import Table from '@/app/ui/invoices/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
-import { lusitana } from '@/app/ui/fonts';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
 import { fetchFilteredCustomers } from '@/app/lib/data';
 import { Metadata } from 'next';
 import CustomersTable from '@/app/ui/customers/table';
